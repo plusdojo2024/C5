d650d880f8d9a9cf69c0d114b230e25fe1452e2c
plusdojo2024-fujimoto <fujimoto-itsuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-14 16:58:00 +0900

    servlet

--- src/servlet/TestServlet.java
+++ src/servlet/TestServlet.java
@@ -9,23 +9,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		//フォワード
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Handbook/testStart.jsp");
-				dispatcher.forward(request, response);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Handbook/testStart.jsp");
+		dispatcher.forward(request, response);
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String ID = request.getParameter("ID");
+		String qize = request.getParameter("qize");
+		String choice_1 = request.getParameter("choice_1");
+		String choice_2 = request.getParameter("choice_2");
+		String choice_3 = request.getParameter("choice_3");
+		String choice_4 = request.getParameter("choice_4");
+		String correct = request.getParameter("correct");
+		String score = request.getParameter("score");
+
 		//フォワード
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Handbook/test.jsp");
-				dispatcher.forward(request, response);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Handbook/test.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
