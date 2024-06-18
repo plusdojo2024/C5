f235a9b889691071b4aca60ec6f7158cea9aa759
plusdojo2024-ono <ono-sumire-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 19:18:48 +0900

    Update BooksDAO.java

--- src/dao/BooksDAO.java
+++ src/dao/BooksDAO.java
@@ -1,7 +1,27 @@
 package dao;
 
-public class BooksDAO {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+import model.Books;
 
 //Books,Channelsテーブルアクセス用DAO。
+
+public class BooksDAO {
+
+	//booksテーブルから、指定したChannel_id(int)のデータ(books)を複数取得する。
+
+	public List<Books> getChannelBooks(int channel_id) {
+		List<Books> booksList = new ArrayList<Books>();
+
+		//SELECT item_name FROM handbook WHERE channel_id = ?;
+
+
+		//SELECT item_name FROM handbook WHERE channel_id = 1;
+		//SELECT item_name FROM handbook WHERE channel_id = 2;
+		//SELECT item_name FROM handbook WHERE channel_id = 3;
+
+		return booksList;
+	}
+
+}
