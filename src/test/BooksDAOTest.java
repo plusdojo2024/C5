852a68a2f083c12dd0e374df0b96ffab8b7116b2
plusdojo2024-ono <ono-sumire-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 14:27:14 +0900

    a

---
+++ src/test/BooksDAOTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class BooksDAOTest
+ */
+@WebServlet("/BooksDAOTest")
+public class BooksDAOTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BooksDAOTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
