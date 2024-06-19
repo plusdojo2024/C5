deefc1c242d9118569a616025f28617bf4273cfc
plusdojo2024-ono <ono-sumire-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 16:02:37 +0900

    Update BooksDAOTest.java

--- src/test/BooksDAOTest.java
+++ src/test/BooksDAOTest.java
@@ -1,21 +1,25 @@
 package test;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import java.util.List;
 
-/**
- * Servlet implementation class BooksDAOTest
- */
-@WebServlet("/BooksDAOTest")
-public class BooksDAOTest extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BooksDAOTest() {
-        super();
-        // TODO Auto-generated constructor stub
+import dao.BooksDAO;
+import model.Books;
+
+public class BooksDAOTest {
+
+    public static void main(String[] args) {
+        BooksDAO booksDAO = new BooksDAO();
+
+        // 仮のチャンネルIDとして1を使用
+        int channelId = 1;
+
+        // BooksDAOからchannelIdに対応する本のリストを取得
+        List<Books> booksList = booksDAO.getChannelBooks(channelId);
+
+        // 取得した本のリストを拡張for文で表示
+        System.out.println("Channel ID は " + channelId + ".衣のページ");
+        for (Books book : booksList) {
+            System.out.println(book.getItem_name()); // Booksクラスに適切なgetItemName()メソッドを仮定
+        }
     }
-
 }
