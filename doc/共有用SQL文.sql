efd37e162ad096566e6909e9d95daf1cafc877a6
plusdojo2024-endo <endo-madoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-27 11:20:16 +0900

    Update 共有用SQL文.sql

--- doc/共有用SQL文.sql
+++ doc/共有用SQL文.sql
@@ -1,3 +1,13 @@
+//ドライバクラス
+org.h2.Driver
+
+//JDBC URL
+jdbc:h2:file:C:/pleiades/workspace/data/C5
+
+//ユーザー名・パスワード
+ユーザー名：sa
+パスワード：pw
+
 //usersテーブル
 create table users(
     id int primary key auto_increment,
@@ -6,6 +16,12 @@ create table users(
     child_birthday date,
     child_name varchar(20)
     );
+
+//ログイン用仮user
+insert into users (id, user_name, password, child_birthday, child_name) values(
+    null, 'ikumi', 'a', '2024-06-27', 'kumi'
+);
+
 //recordsテーブル
 create table records(
     id int primary key auto_increment,
@@ -13,6 +29,7 @@ create table records(
     date date(8),
     temperature varchar(4)
 );
+
 //record_mealsテーブル
 create table record_meals(
     meal_id int primary key auto_increment,
@@ -24,6 +41,7 @@ create table record_meals(
     meal_comment varchar(100),
     meal_timestamp timestamp
 );
+
 //record_sleepsテーブル
 create table record_sleeps(
     sleep_id int primary key auto_increment,
@@ -32,6 +50,7 @@ create table record_sleeps(
     sleep_comment varchar(100),
     sleep_timestamp timestamp
 );
+
 //record_excretionsテーブル
 create table record_excretions(
     excretion_id int primary key auto_increment,
@@ -42,6 +61,7 @@ create table record_excretions(
     pee boolean,
     excretion_timestamp timestamp
 );
+
 /record_commentsテーブル
 create table record_comments(
     comment_id int primary key auto_increment,
@@ -49,17 +69,38 @@ create table record_comments(
     comment varchar(200),
     comment_timestamp timestamp
 );
+
+//photosテーブル
+create table photos(
+    id int primary key auto_increment,
+    user_id int,
+    img_timestamp timestamp,
+    imgPath1 varchar(200),
+    imgPath2 varchar(200),
+    imgPath3 varchar(200),
+    imgPath4 varchar(200),
+    imgPath5 varchar(200)   
+);
+
 //consulsテーブル
 create table consuls(
     id int primary key auto_increment,
     user_id int,
     channel_id int,
-    post_id int,
-    post_number int,
-    post_content varchar(500),
-    post_time timestamp
+    consul_content varchar(100),
+    consul_time timestamp
 );
-//booksテーブル(小野ちゃん変更お願いします)
+
+//commentsテーブル
+create table comments(
+    id int primary key auto_increment,
+    user_id int,
+    consul_id int,
+    comment_content varchar(100),
+    comment_time timestamp
+); 
+
+//booksテーブル
 CREATE TABLE handbook (
 id int AUTO_INCREMENT(1) ,
 channel_id int,
@@ -69,12 +110,14 @@ item_id int,
 explanation VARCHAR(1000),
  PRIMARY KEY (id)
 );
+
 //channelsテーブル
 create table channels(
     id int primary key auto_increment,
     channel_name varchar,
     channel_color varchar(100)
 );
+
 //gamesテーブル
 create table games(
     id int primary key,
@@ -86,14 +129,102 @@ create table games(
     correct varchar(1),
     score int
 );
-//photosテーブル
-create table photos(
-    id int primary key auto_increment,
-    user_id int,
-    img_timestamp timestamp,
-    imgPath1 varchar(200),
-    imgPath2 varchar(200),
-    imgPath3 varchar(200),
-    imgPath4 varchar(200),
-    imgPath5 varchar(200)   
+
+//gamesテーブルデータ
+INSERT INTO Games  (id, quiz, choice_1, choice_2, choice_3, choice_4, correct, score)VALUES (
+'1',
+'おむつを替える手順として正しい順番は次の打つどれですか？1新しいおむつをあてる
+2新しいおむつを敷く
+3汚れたおむつを引き抜く
+4おむつを外しておしりを拭く
+
+
+',
+'3→1→4→2',
+
+'1→2→3→4
+',
+
+'2→4→3→1',
+
+'2→1→4→3
+',
+
+'3',
+'10'
+);
+
+
+INSERT INTO Games  (id, quiz, choice_1, choice_2, choice_3, choice_4, correct, score)VALUES (
+'2',
+'赤ちゃんを外出させる際に持参すべきものは次のうちどれですか？
+',
+'おむつ',
+'ミルク',
+'おしりふき',
+'すべて',
+'4',
+'10'
+);
+
+
+
+INSERT INTO Games  (id, quiz, choice_1, choice_2, choice_3, choice_4, correct, score)VALUES (
+'3',
+'赤ちゃんに食べさせてはいけない食べ物は？
+',
+'にんじん',
+'とうふ',
+'さつまいも',
+'はちみつ',
+'4',
+'10'
+);
+
+INSERT INTO Games  (id, quiz, choice_1, choice_2, choice_3, choice_4, correct, score)VALUES (
+'4',
+'赤ちゃんが食べるべきでない栄養素は次のうちどれですか？
+',
+'タンパク質',
+'ビタミンC',
+'鉄分',
+'過剰な塩分',
+'4',
+'10'
+);
+
+INSERT INTO Games  (id, quiz, choice_1, choice_2, choice_3, choice_4, correct, score)VALUES (
+'5',
+'赤ちゃんにミルクを与える際、次のうちどの方法が適切ですか？',
+'食前に一度に多く与える',
+'食前に少しずつ与える',
+'食後に少しずつこまめに与える',
+'食事と同時に与える',
+'3',
+'10'
+);
+
+INSERT INTO Games  (id, quiz, choice_1, choice_2, choice_3, choice_4, correct, score)VALUES (
+'6',
+'粉ミルクを溶かすときのお湯は何度？
+',
+'0度',
+'15度',
+'70度以上',
+'決まっていない',
+'3',
+'10'
+);
+
+INSERT INTO Games  (id, quiz, choice_1, choice_2, choice_3, choice_4, correct, score)VALUES (
+'7',
+'赤ちゃんがいる家庭で避けるべき家具は次のうちどれですか?',
+'安全ロック機能の付いたベビーゲート
+',
+'高さの低い安定したテーブル
+',
+'小さな隙間のないベッド',
+'尖った角のあるテーブル',
+'4',
+'10'
 );
